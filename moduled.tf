4decabb38dc2db792484b39a37c1b34dd57b8037
kbalow <kbalow@gmail.com>
2020-03-30 21:52:02 -0500

    commit

--- moduled.tf
+++ moduled.tf
@@ -1,25 +1,15 @@
-module "ec2-instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
+module "ec2_instance" {
+  source  = "app.terraform.io/Balow/ec2-instance/aws"
   version = "2.13.0"
-  # insert the 10 required variables here
-  ami = "${var.ami}"
-  associate_public_ip_address = true
-  instance_type = "${var.instance_type}"
-  ipv6_address_count = 0
-  ipv6_addresses = {}
-  name = "foundryvtt"
-  private_ip = {}
-  user_data = {}
-  user_data_base64 = {}
-  vpc_security_group_ids = {}
-}
 
-module "web_server_sg" {
-  source = "terraform-aws-modules/security-group/aws//modules/http"
-
-  name        = "web-server"
-  description = "Security group for web-server with HTTP ports open within VPC"
-  vpc_id      = "vpc-12345678"
-
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ami = "ami-07ebfd5b3428b6f4d"
+  associate_public_ip_address = "true"
+  instance_type = "t2.micro"
+  ipv6_address_count = "${var.ec2_instance_ipv6_address_count}"
+  ipv6_addresses = "${var.ec2_instance_ipv6_addresses}"
+  name = "foundry_server"
+  private_ip = "${var.ec2_instance_private_ip}"
+  user_data = "${var.ec2_instance_user_data}"
+  user_data_base64 = "${var.ec2_instance_user_data_base64}"
+  vpc_security_group_ids = "${var.ec2_instance_vpc_security_group_ids}"
 }
--- variables.tf
+++ variables.tf
@@ -1,23 +1,6 @@
-variable "aws_private_key_path" {
-  default = "~/.ssh/koryb_puttyGen.pem"
-}
-
-variable "aws_key_name" {
-  default = "koryb_puttyGen"
-}
-
-variable "aws_region" {
-  default = "us-east-2"
-}
-
-variable "aws_vm_size" {
-  default = "t2.micro"
-}
-
-variable "aws_ami" {
-  default = "ami-05c1fa8df71875112"
-}
-
-variable "record_name" {
-  default = "www.balow.me"
-}
+variable "ec2_instance_ipv6_address_count" {}
+variable "ec2_instance_ipv6_addresses" {}
+variable "ec2_instance_private_ip" {}
+variable "ec2_instance_user_data" {}
+variable "ec2_instance_user_data_base64" {}
+variable "ec2_instance_vpc_security_group_ids" {}
