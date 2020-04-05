2cfc4eceb07c8bdb898f9a791b43756cf4ec1ae9
kbalow <kbalow@gmail.com>
2020-04-05 11:10:18 -0500

    commit

--- variables.tf
+++
@@ -1,6 +0,0 @@
-variable "ec2_instance_ipv6_address_count" {}
-variable "ec2_instance_ipv6_addresses" {}
-variable "ec2_instance_private_ip" {}
-variable "ec2_instance_user_data" {}
-variable "ec2_instance_user_data_base64" {}
-variable "ec2_instance_vpc_security_group_ids" {}
