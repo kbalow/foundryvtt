3b223c2d0d634f49023ff03514a98fb2f1623bdb
kbalow <kbalow@gmail.com>
2020-04-05 11:09:12 -0500

    moduled

--- moduled.tf
+++ moduled.tf
@@ -1,15 +1,37 @@
-module "ec2_instance" {
-  source  = "app.terraform.io/Balow/ec2-instance/aws"
-  version = "2.13.0"
+provider "aws" {
+  region = "us-west-2"
+}
 
-  ami = "ami-07ebfd5b3428b6f4d"
-  associate_public_ip_address = "true"
-  instance_type = "t2.micro"
-  ipv6_address_count = "${var.ec2_instance_ipv6_address_count}"
-  ipv6_addresses = "${var.ec2_instance_ipv6_addresses}"
-  name = "foundry_server"
-  private_ip = "${var.ec2_instance_private_ip}"
-  user_data = "${var.ec2_instance_user_data}"
-  user_data_base64 = "${var.ec2_instance_user_data_base64}"
-  vpc_security_group_ids = "${var.ec2_instance_vpc_security_group_ids}"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.21.0"
+
+  name = "foundry_vtt"
+  cidr = "0.0.0.0/0"
+
+  #azs             = var.vpc_azs
+  #private_subnets = var.vpc_private_subnets
+  #public_subnets  = var.vpc_public_subnets
+
+  #enable_nat_gateway = var.vpc_enable_nat_gateway
+
+  #tags = var.vpc_tags
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "2.12.0"
+
+  name           = "foundry_vtt"
+  instance_count = 1
+
+  ami                    = "ami-07ebfd5b3428b6f4d"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "foundry"
+  }
 }
