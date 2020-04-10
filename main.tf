201f3da24245bcf6fd65abc6686698a00be249f9
kbalow <kbalow@gmail.com>
2020-04-10 18:53:12 -0500

    commit

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   profile    = "default"
   region     = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
 
 resource "aws_instance" "foundry" {
