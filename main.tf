52e0db424e06788156db70d288be614d9a57a895
kbalow <kbalow@gmail.com>
2020-04-18 11:09:52 -0500

    commit

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "aws_instance" "foundry" {
   }
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  security_groups = ["ssh","http_https"]
+  security_groups = ["ssh","http_https","foundryvtt"]
   key_name = "foundry_vtt"
   tags = {
       Name = "foundry"
@@ -49,10 +49,7 @@ resource "aws_instance" "foundry" {
       "wget ${var.foundry_link} -O foundryvtt.zip",
       "unzip foundryvtt.zip",
       "sudo service nginx start",
-      "node resources/app/main.js --dataPath=$HOME/foundrydata",
-      "sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu",
-      "pm2 startup",
-      "pm2 start /home/ubuntu/foundry/resources/app/main.js --name \"foundry\" -- --port=8080",
+      "node main.js --port=30000 --world=myworld --dataPath=/local/data/foundryvtt"
     ]
   }
 }
